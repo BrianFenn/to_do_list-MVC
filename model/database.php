8a4c6fcb90ac69c2e90e3ebfd1b3cd7b53685934
BrianFenn <34203314+BrianFenn@users.noreply.github.com>
2020-03-04 19:49:42 -0800

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,8 +1,8 @@
 <?php
-    $dsn = 'erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-    $username = 'ps6tp9byxkqga18a';
-    $password = 'ikhd47q0kgp7lsz2';
-    $dbname = 'xr7mltv9xu2cv4eo';
+    $dsn = "erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $username = "ps6tp9byxkqga18a";
+    $password = "ikhd47q0kgp7lsz2";
+    $dbname = "xr7mltv9xu2cv4eo";
 
     try {
         $db = new PDO($dsn, $username, $password, $dbname);
