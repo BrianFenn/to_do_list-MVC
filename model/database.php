71f59637f86ea5d9071e813a52b13224bdcb61e7
BrianFenn <34203314+BrianFenn@users.noreply.github.com>
2020-03-04 19:58:04 -0800

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,5 +1,5 @@
 <?php
-    $dsn = "erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=xr7mltv9xu2cv4eo";
+    $dsn = "mysql:host=erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=xr7mltv9xu2cv4eo";
     $username = "ps6tp9byxkqga18a";
     $password = "ikhd47q0kgp7lsz2";
     //$dbname = "xr7mltv9xu2cv4eo";
